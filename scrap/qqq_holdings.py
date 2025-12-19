a757e0b1fdb2c52619c079b2eea83041d70d2474
Septem Li <1216949+septemhill@users.noreply.github.com>
2025-12-19 09:56:53 +0800

    update qqq scrap script

--- scrap/qqq_holdings.py
+++ scrap/qqq_holdings.py
@@ -3,6 +3,7 @@ import requests
 import time
 import json
 from dataclasses import dataclass
+from quickchart import QuickChart
 
 
 @dataclass
@@ -38,4 +39,74 @@ class QQQHoldingsScraper:
             for rate in holdings_data
         ]
 
-        self.notifier.thread_notify(title, content, holdings)
+        newHoldings = self.data_filter(holdings)
+
+        companies = [h.ticker for h in newHoldings]
+        percents = [h.percentageOfTotalNetAssets for h in newHoldings]
+
+        qc = QuickChart()
+        qc.width = 1600
+        qc.height = 1200
+        qc.config = f"""{{
+            type: 'doughnut',
+            data: {{
+                labels: {companies},
+                datasets: [
+                    {{
+                        backgroundColor: [
+                            "#a6cee3",
+                            "#1f78b4",
+                            "#b2df8a",
+                            "#33a02c",
+                            "#fb9a99",
+                            "#e31a1c",
+                            "#fdbf6f",
+                            "#ff7f00",
+                            "#cab2d6",
+                            "#6a3d9a",
+                            "#ffff99",
+                            "#b15928",
+                        ],
+                        data: {percents}
+                    }}
+                ]
+            }},
+            options: {{
+                plugins: {{
+                    colorschemes: {{scheme: 'brewer.Paired12'}},
+                    legend: true,
+                    doughnutlabel: {{
+                        labels: [
+                            [{{
+                                font: {{
+                                    size: 5
+                                }}
+                            }}]
+                        ]
+                    }}
+                }}
+            }}
+        }}"""
+
+        self.notifier.notify(title, content, qc.get_url())
+
+    def data_filter(
+        self, holdings: list[Holding], percent_threshold: float = 40.0
+    ) -> list[Holding]:
+        sorted_holdings = sorted(
+            holdings, key=lambda h: h.percentageOfTotalNetAssets, reverse=True
+        )
+        new_holdings: list[Holding] = []
+        cumulative_percent = 0.0
+        other_holdings: list[Holding] = []
+        for holding in sorted_holdings:
+            if cumulative_percent < percent_threshold:
+                new_holdings.append(holding)
+                cumulative_percent += holding.percentageOfTotalNetAssets
+            else:
+                other_holdings.append(holding)
+        other_weight = sum(h.percentageOfTotalNetAssets for h in other_holdings)
+        new_holdings.append(
+            Holding(ticker="Other", percentageOfTotalNetAssets=other_weight)
+        )
+        return new_holdings
